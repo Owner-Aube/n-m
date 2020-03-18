5e540c95106e643df276aec2776c2533dabe8a15
Owner-Aube <58786143+Owner-Aube@users.noreply.github.com>
2020-03-18 23:35:07 +0100

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -1,7 +1,7 @@
 module.exports = (client) => {
     client.user.setPresence({
         game: {
-            name: "Regarder Division."
+            name: "Naruto RP v5 -> 50h de taff."
         }
     });
 };
