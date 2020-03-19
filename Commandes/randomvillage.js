66dbdb589632e0f9369e11a6e36a155eadca963d
Owner-Aube <58786143+Owner-Aube@users.noreply.github.com>
2020-03-19 23:04:28 +0100

    Update randomvillage.js

--- Commandes/randomvillage.js
+++ Commandes/randomvillage.js
@@ -13,7 +13,7 @@ module.exports.run = (client, message, args) => {
 
     const answer = answers[Math.floor(Math.random() * answers.length)];
     
-    msg.reply("\n ``` ```\n **__Konoha (木ノ葉隠れの里)__**\n \`\`\`diff\n- Konoha (木ノ葉隠れの里, Konohagakure no Sato) est le village caché de la Feuille. Tout comme les villages cachés des cinq autres grands pays, le village de Konoha est dirigé par un kage, en l'occurrence le Hokage. Le bandeau frontal des ninjas de Konoha est frappé d'une feuille stylisée. Cette feuille symbolise les méthodes d'apprentissage qui étaient dispensées aux aspirants ninjas des précédentes générations : une feuille était posée sur leurs fronts et ils devaient la faire tourner afin d'accroître leur concentration et de développer leur maîtrise de soi.\`\`\`\n ``` ``` \n https://media1.tenor.com/images/bcfbaa4f6dbce9e605775c5cb927ad44/tenor.gif?itemid=14801470")
+    message.reply("\n ``` ```\n **__Konoha (木ノ葉隠れの里)__**\n \`\`\`diff\n- Konoha (木ノ葉隠れの里, Konohagakure no Sato) est le village caché de la Feuille. Tout comme les villages cachés des cinq autres grands pays, le village de Konoha est dirigé par un kage, en l'occurrence le Hokage. Le bandeau frontal des ninjas de Konoha est frappé d'une feuille stylisée. Cette feuille symbolise les méthodes d'apprentissage qui étaient dispensées aux aspirants ninjas des précédentes générations : une feuille était posée sur leurs fronts et ils devaient la faire tourner afin d'accroître leur concentration et de développer leur maîtrise de soi.\`\`\`\n ``` ``` \n https://media1.tenor.com/images/bcfbaa4f6dbce9e605775c5cb927ad44/tenor.gif?itemid=14801470")
 
 };
 
