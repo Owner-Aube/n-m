c1493aa1649ec207ad644997a798dc3b120625b4
Owner-Aube <58786143+Owner-Aube@users.noreply.github.com>
2020-03-19 21:23:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,44 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 client.commands = new Discord.Collection()
 const fs = require('fs');
+let db = JSON.parse(fs.readFileSync("./database.json", "utf8"));
+
+client.on("message", message => {
+    if (message.author.bot) return;
+
+
+    if (!db[message.author.id]) db[message.author.id] = {
+        xp: 0,
+        level: 0
+      };
+    db[message.author.id].xp++;
+    let userInfo = db[message.author.id];
+    if(userInfo.xp > 100) {
+        userInfo.level++
+        userInfo.xp = 0
+        message.reply("Félicitation ! Tu viens de monté de 1 niveau !")
+    }
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if(cmd === "info") {
+        let userInfo = db[message.author.id];
+        let member = message.mentions.members.first();
+        let embed = new Discord.RichEmbed()
+        .setColor(0x4286f4)
+        .addField("Level", userInfo.level)
+        .addField("XP", userInfo.xp+"/100");
+        if(!member) return message.channel.send(embed)
+        let memberInfo = db[member.id]
+        let embed2 = new Discord.RichEmbed()
+        .setColor(0x4286f4)
+        .addField("Level", memberInfo.level)
+        .addField("XP", memberInfo.xp+"/100")
+        message.channel.send(embed2)
+    }
+    fs.writeFile("./database.json", JSON.stringify(db), (x) => {
+        if (x) console.error(x)
+      });
+})
 
 client.on('message', message => {
 if(message.content.startsWith('.launchcodejavascript'))
